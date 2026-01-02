9cc7008ac3b3700cf553879f5e55c7e7be78b720
Reyaansh Sinha <reyaanshsinha4@gmail.com>
2026-01-02 00:40:38 -0500

    Delete SpliceBlocks directory

--- SpliceBlocks/core/any_aliases.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @license
- * Copyright 2025 Sinha Group
- * SPDX-License-Identifier: Apache-2.0
- */
-
-// eslint-disable-next-line
-type AnyDuringMigration = any;
--- SpliceBlocks/typings/php.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @license
- * Copyright 2025 Sinha Group
- * SPDX-License-Identifier: Apache-2.0
- */
-
-export * from './generators/php';
